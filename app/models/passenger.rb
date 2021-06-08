4d23074eeffa27a285756336dd2ae4040fc8ce64
Marc Nopprapun <mnopprapun@gmail.com>
2021-06-08 10:30:26 -0500

    commit tree 10d6036183c3d60f708ca82007bf20f7308aebbb

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,6 @@
-class Passenger < ApplicationRecord
+class Passenger < ApplicationRecord	
+  	
+	has_many :rides
+has_many :taxis, through: :rides
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,8 @@
 class Ride < ApplicationRecord
+
+	belongs_to :taxi
+	belongs_to :passenger
+	
 end
+
+
